7cd52e0b3f9dbfc8c8b2eaffd4f11595cbaa5b1e
Yerkebulan Shakirbekov <erkebulanshakirbekov@gmail.com>
2021-02-11 17:16:43 +0600

    add Header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import styles from './Header.module.scss';
+
+
+const Header: React.FC = () => { 
+
+    return (
+       <div className = {styles.Header}>
+          <header>
+            <div className={styles.container}>
+                
+            </div>
+          </header>
+       </div>
+    )
+
+}
+
+export default Header;
